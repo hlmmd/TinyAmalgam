f488a171e9c70a575f14c14298ae545f566e1569
hlmmd <hlmmd1995@gmail.com>
2021-06-26 09:46:02 +0800

    add str.h

---
+++ algorithm/str.cc
@@ -0,0 +1,114 @@
+
+#include "str.h"
+#include <vector>
+namespace hl
+{
+namespace algorithm
+{
+namespace string
+{
+
+int SubStr(const std::string& text, const std::string& pattern)
+{
+    auto getNext = [](const std::string& pattern) -> std::vector<int>
+    {
+        int n = pattern.size();
+        std::vector<int> next(n, 0);
+        int prefixlen = 0;
+        for (int i = 1; i < n; i++)
+        {
+            if (pattern[i] == pattern[prefixlen])
+            {
+                next[i] = ++prefixlen;
+            }
+            else
+            {
+                if (prefixlen > 0)
+                {
+                    prefixlen = next[prefixlen - 1];
+                    i--;
+                }
+                else
+                    next[i] = 0;
+            }
+        }
+
+        return next;
+    };
+
+    auto next = getNext(pattern);
+    for (int i = 0, j = 0; i < text.size(); i++)
+    {
+        if (text[i] == pattern[j])
+        {
+            j++;
+            if (j == pattern.size())
+                return i - j;
+        }
+        else
+        {
+            j = next[j];
+            if (j != 0)
+                i--;
+        }
+    }
+    return -1;
+}
+} // namespace string
+} // namespace algorithm
+} // namespace hl
+
+class Solution
+{
+public:
+    int strStr(std::string text, std::string pattern)
+    {
+        if (pattern.size() == 0)
+            return 0;
+        auto getNext = [](const std::string& pattern) -> std::vector<int>
+        {
+            int n = pattern.size();
+            std::vector<int> next(n, 0);
+            int prefixlen = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (pattern[i] == pattern[prefixlen])
+                {
+                    next[i] = ++prefixlen;
+                }
+                else
+                {
+                    if (prefixlen > 0)
+                    {
+                        prefixlen = next[prefixlen - 1];
+                        i--;
+                    }
+                    else
+                        next[i] = 0;
+                }
+            }
+
+            return next;
+        };
+
+        auto next = getNext(pattern);
+        for (int i = 0, j = 0; i < text.size(); i++)
+        {
+            if (text[i] == pattern[j])
+            {
+                j++;
+                if (j == pattern.size())
+                    return i - j + 1;
+            }
+            else
+            {
+                if (j != 0)
+                {
+                    j = next[j - 1];
+                    i--;
+                }
+            }
+        }
+        return -1;
+    }
+};
---
+++ algorithm/str.h
@@ -0,0 +1,29 @@
+/*================================================================
+*   Copyright (C) 2021 hlmmd. All rights reserved.
+*
+*   FileName    : string.h
+*   Autor       ：hlmmd
+*   Date        : 2021/06/25
+*   Description :
+*
+================================================================*/
+
+#ifndef ALGORITHM_STRING_H
+#define ALGORITHM_STRING_H
+
+#include <string>
+
+namespace hl
+{
+namespace algorithm
+{
+namespace string
+{
+using namespace std;
+int SubStr(const std::string& text, const std::string& pattern);
+
+} // namespace string
+} // namespace algorithm
+} // namespace hl
+
+#endif
